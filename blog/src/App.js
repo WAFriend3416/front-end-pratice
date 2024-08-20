7a686c7b4168eeb9647db99e851fdec413fb0c0e
waf <wafriend1031@gmail.com>
2024-08-20 17:25:17 +0900

    리액트 배우기 1일차-1

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+
 
 function App() {
+
+  let post = '강남 우동 맛집';
+  let[logo,setLogo] = useState('ReactBlog') // 이런 블로그 제목은 굳이 state?
+  let[글제목목록,b] = useState(['남자 코드 추천',
+                              'JS 너무 신기해',
+                              '아무튼 추천']) // ['남자 코드 추천' , 함수]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-dev">
+        <h4>{logo}</h4>
+      </div>1
+      <div className="list">
+        <h4>{글제목목록[0]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목목록[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목목록[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
